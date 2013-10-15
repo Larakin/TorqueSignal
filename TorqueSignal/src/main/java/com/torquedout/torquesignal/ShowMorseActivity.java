e53d3e299aed4836ce5aaad074914dc65fa67637
Justin Erickson <larakinsystems@gmail.com>
2013-10-14 23:15:40 -0600

    Added code Hash and initial converter

--- TorqueSignal/src/main/java/com/torquedout/torquesignal/ShowMorseActivity.java
+++ TorqueSignal/src/main/java/com/torquedout/torquesignal/ShowMorseActivity.java
@@ -2,15 +2,21 @@ package com.torquedout.torquesignal;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.content.Intent;
 import android.widget.TextView;
+import android.util.Log;
+
+import java.util.Hashtable;
 
 public class ShowMorseActivity extends Activity {
+
+    private static final String TAG = "ShowMorse";
+
+    private Hashtable code = new Hashtable();
 
     //@SuppressLint("NewApi")
     @Override
@@ -22,12 +28,68 @@ public class ShowMorseActivity extends Activity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String dots;
+
+        fillCodeHash();
+        dots = convertMessage(message);
 
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText(message);
+        textView.setText(dots);
 
         setContentView(textView);
+    }
+
+    private void fillCodeHash() {
+        //TODO: Better way to do this!
+        code.put("A",".-");
+        code.put("B","-...");
+        code.put("C","-.-.");
+        code.put("D","-..");
+        code.put("E",".");
+        code.put("F","..-.");
+        code.put("G","--.");
+        code.put("H","....");
+        code.put("I","..");
+        code.put("J",".---");
+        code.put("K","-.-");
+        code.put("L",".-..");
+        code.put("M","--");
+        code.put("N","-.");
+        code.put("O","---");
+        code.put("P",".--.");
+        code.put("Q","--.-");
+        code.put("R",".-.");
+        code.put("S","...");
+        code.put("T","-");
+        code.put("U","..-");
+        code.put("V","...-");
+        code.put("W",".--");
+        code.put("X","-..-");
+        code.put("Y","-.--");
+        code.put("Z","--..");
+
+        code.put("1",".----");
+        code.put("2","..---");
+        code.put("3","...--");
+        code.put("4","....-");
+        code.put("5",".....");
+        code.put("6","-....");
+        code.put("7","--...");
+        code.put("8","---..");
+        code.put("9","----.");
+        code.put("0","-----");
+    }
+
+    private String convertMessage(String text) {
+        String dots = "";
+
+        for  (char ch: text.toUpperCase().toCharArray()) {
+            Log.v(TAG, "Char:" + ch);
+            dots += code.get(String.valueOf(ch)) + " ";
+        }
+
+        return dots;
     }
 
     /**
